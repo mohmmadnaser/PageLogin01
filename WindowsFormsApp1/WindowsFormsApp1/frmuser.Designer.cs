fc648a6f1c65f6fa0e40d62d4d15f2e9bf5da14c
MOHMMAD NASER <127879571+mohmmadnaser@users.noreply.github.com>
2023-04-05 23:41:38 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/frmuser.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmuser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.lblusername = new System.Windows.Forms.Label();
+            this.lblpasswprd = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(333, 77);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(214, 28);
+            this.txtusername.TabIndex = 0;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(333, 160);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '*';
+            this.txtpassword.Size = new System.Drawing.Size(214, 28);
+            this.txtpassword.TabIndex = 1;
+            // 
+            // lblusername
+            // 
+            this.lblusername.AutoSize = true;
+            this.lblusername.Location = new System.Drawing.Point(210, 83);
+            this.lblusername.Name = "lblusername";
+            this.lblusername.Size = new System.Drawing.Size(106, 22);
+            this.lblusername.TabIndex = 2;
+            this.lblusername.Text = "User name";
+            // 
+            // lblpasswprd
+            // 
+            this.lblpasswprd.AutoSize = true;
+            this.lblpasswprd.Location = new System.Drawing.Point(213, 165);
+            this.lblpasswprd.Name = "lblpasswprd";
+            this.lblpasswprd.Size = new System.Drawing.Size(97, 22);
+            this.lblpasswprd.TabIndex = 3;
+            this.lblpasswprd.Text = "Password";
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(333, 228);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(112, 33);
+            this.btnlogin.TabIndex = 4;
+            this.btnlogin.Text = "Log In";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // frmuser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(744, 317);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.lblpasswprd);
+            this.Controls.Add(this.lblusername);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusername);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmuser";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmuser_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Label lblusername;
+        private System.Windows.Forms.Label lblpasswprd;
+        private System.Windows.Forms.Button btnlogin;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/frmuser.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmuser : Form
+    {
+        public frmuser()
+        {
+            InitializeComponent();
+        }
+
+        private User user1;
+        private User user2;
+        private User user3;
+        
+
+
+        public List<User> users;
+
+
+        private void frmuser_Load(object sender, EventArgs e)
+        {
+            user1 = new User();
+            user1.UserId = 1;
+            user1.FullName = "mohmmad naser";
+            user1.Username = "MN";
+            user1.Password = "1991";
+
+            user2 = new User();
+            user2.UserId = 2;
+            user2.FullName = "ahmmad ";
+            user2.Username = "AA";
+            user2.Password = "5678";
+
+            user3 = new User();
+            user3.UserId = 3;
+            user3.FullName = "mask ";
+            user3.Username = "MA";
+            user3.Password = "2020";
+
+
+
+            users = new List<User>();
+
+            users.Add(user1);
+            users.Add(user2);
+            users.Add(user3);
+            
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+
+
+            
+
+             if (user1.Password == txtpassword.Text)
+            { 
+
+                    MessageBox.Show("Welcome " + user1.FullName);
+            }
+             else if (user2.Password == txtpassword.Text)
+            {
+
+                MessageBox.Show("Welcome " + user2.FullName);
+            }
+             else if (user3.Password == txtpassword.Text)
+            {
+
+                MessageBox.Show("Welcome " + user3.FullName);
+            }
+
+             else if (user1.Username != txtusername.Text)
+            {
+                MessageBox.Show("User does not exist");
+
+            }
+             else if (user2.Username != txtusername.Text)
+            {
+                MessageBox.Show("User does not exist");
+
+            }
+            else if (user3.Username != txtusername.Text)
+            {
+                MessageBox.Show("User does not exist");
+
+            }
+            else
+                {
+                    MessageBox.Show("Wrong password");
+                }
+
+
+
+            }
+
+
+        }
+    }
+
+        
+
+
+
+
+
+
+
